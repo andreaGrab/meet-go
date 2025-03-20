c590fb4db7a57b3dd14497af45267fbd04b83408
Andrea Grabovac <ag@192.168.1.7>
2025-03-20 15:09:36 +0100

    added slideshow logic for current event gallery

--- src/app/components/event-carousel.tsx
+++ src/app/components/event-carousel.tsx
@@ -1,44 +1,104 @@
+"use client";
 // sidenotes:
 // larger event container width after viewport:1754px
+// dummy data
+const testoMatto:string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium distinctio soluta quo, aperiam ad fugiat sequi quae. Consequuntur, tempora labore, pariatur ex a nisi nesciunt sint unde dolore nostrum asperiores?';
+
+const events:Array<any> = [{
+    title: 'Concert and Elegance Festival.',
+    description: testoMatto,
+    date: '10/12/2025',
+    location: 'Grand Auditorium, Paris, France',
+    organizer: 'Limbo Life ltd',
+    soldTickets: 739,
+    price: '12,99€',
+    gallery: [
+        '/gallery_testing/event1/conductor.jpg',
+        '/gallery_testing/event1/champagne.jpg',
+        '/gallery_testing/event1/violin.jpg'
+    ]
+}, {
+    title: 'Wedding on the beach!',
+    description: testoMatto,
+    date: '15/08/2025',
+    location: 'Jesolo, Venice, Italy',
+    organizer: 'Rossana DKW snc',
+    soldTickets: 320,
+    price: 'FREE',
+    gallery: [
+        '/gallery_testing/event2/beach.jpg',
+        '/gallery_testing/event2/fireworks.jpg',
+        '/gallery_testing/event2/table.jpg'
+    ]
+}, {
+    title: 'Music party event',
+    description: testoMatto,
+    date: '07/06/2025',
+    location: 'Alpha Club, Berlin, Germany',
+    organizer: 'Alpha Club',
+    soldTickets: 739,
+    price: '35,00€',
+    gallery: [
+        '/gallery_testing/event3/hand.jpg',
+        '/gallery_testing/event3/audience.jpg',
+        '/gallery_testing/event3/music.jpg'
+    ]
+}];
+// ------"
+
+import { useState, useEffect } from "react";
 
 export default function EventCarousel(){
-    return <div className="h-fit bg-[url(/gallery_testing/event1/conductor.jpg)] bg-center lg:bg-right overflow-hidden flex flex-col items-center">
+    const [index, setIndex] = useState(0);
+    const eventLenGallery = events[0].gallery.length;
+    function nextSlide(){
+        const nextSlide = (index+1)%eventLenGallery;
+        setIndex(nextSlide);
+    }
+    useEffect(()=>{
+        const timer = setInterval(()=>{
+            nextSlide();
+        }, 3000);
+        return ()=>clearInterval(timer);
+    }, [index]);
+
+    return <div style={{'--dynamic-image': `url(${events[0].gallery[index]})`}} className="h-fit bg-[image:var(--dynamic-image)] bg-center lg:bg-right overflow-hidden flex flex-col items-center">
         <div className="lg:backdrop-blur-3xl w-[100%]">
-            <div className="flex md:w-[85%] 2xl:w-[60%] h-full m-auto bg-white/80 p-5 gap-5 rounded-b-sm">
+            <div className="flex md:drop-shadow-sm md:w-[85%] 2xl:w-[60%] h-full m-auto bg-white/80 p-5 gap-5 rounded-b-sm">
                 <div className="flex-1">
                     <div className="lg:max-w-96 overflow-hidden">
-                        <h1 className="text-[2rem]/12 md:text-[2.5rem] font-bold">Name event very long title</h1>
+                        <h1 className="text-[2rem]/12 md:text-[2.5rem] font-bold">{events[0].title}</h1>
                     </div>
-                    <p className="my-3 max-h-[145px] overflow-hidden">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium distinctio soluta quo, aperiam ad fugiat sequi quae. Consequuntur, tempora labore, pariatur ex a nisi nesciunt sint unde dolore nostrum asperiores?</p>
+                    <p className="my-3 max-h-[145px] overflow-hidden">{events[0].description}</p>
 
                     <div className="grid grid-rows-2 grid-cols-2 gap-1 md:my-5">
                         <div className="flex items-center gap-2">
                             <img className="w-5" src="/calendar.svg" alt="calendar icon" />
-                            <p>10/12/2025</p>
+                            <p>{events[0].date}</p>
                         </div>
                         <div className="flex items-center gap-2">
                             <img className="w-5" src="/map-pin.svg" alt="location icon" />
-                            <p>Location</p>
+                            <p>{events[0].location}</p>
                         </div>
                 
                         <div className="flex items-center gap-2">
                             <img className="w-5" src="/face-laugh.svg" alt="smile icon" />
-                            <p>Organizer</p>
+                            <p>{events[0].organizer}</p>
                         </div>
                         <div className="flex items-center gap-2">
                             <img className="w-5" src="/users.svg" alt="smile icon" />
-                            <p>257</p>
+                            <p>{events[0].soldTickets}</p>
                         </div>
                         
                     </div>
                     <div className="flex flex-col xl:flex-row gap-5 my-5">
                         <button className="border-1 border-blue-400 drop-shadow-md active:shadow-[inset_0px_1px_1px_rgba(0,0,0,0.3)] bg-slate-50 p-2 rounded-sm cursor-pointer hover:bg-slate-100 hover:drop-shadow-sm transition-all ease-in duration-75 active:drop-shadow-none active:text-black active:translate-y-[1px]">VIEW MORE</button>
-                        <button className="border-1 border-blue-400 drop-shadow-md active:shadow-[inset_0px_1px_1px_rgba(0,0,0,0.3)] bg-slate-50 p-2 rounded-sm cursor-pointer hover:bg-slate-100 hover:drop-shadow-sm transition-all ease-in duration-75 active:drop-shadow-none active:text-black active:translate-y-[1px]">BOOK NOW FOR 12,99€</button>
+                        <button className="border-1 border-blue-400 drop-shadow-md active:shadow-[inset_0px_1px_1px_rgba(0,0,0,0.3)] bg-slate-50 p-2 rounded-sm cursor-pointer hover:bg-slate-100 hover:drop-shadow-sm transition-all ease-in duration-75 active:drop-shadow-none active:text-black active:translate-y-[1px]">BOOK NOW FOR {events[0].price}</button>
                     </div>
                 </div>
                 <div className="flex-2 hidden lg:block">
                     <div className="overflow-hidden w-[100%] h-[100%] relative">
-                        <img className="absolute w-[100%]" src="/gallery_testing/event1/conductor.jpg" alt="event image" />
+                        <img className="absolute w-[100%]" src={(events[0].gallery[index])} alt="event image" />
                     </div>
                 </div>
             </div>
---
+++ src/app/modules/carousel_logic.ts
@@ -0,0 +1,6 @@
+
+export default function caroHydration(events:object[]){
+    events.forEach(event=>{
+        console.log(event)
+    });
+}
