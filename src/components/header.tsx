1e0a8bb351c36a7e790d25c82092485c215a9c41
Andrea Grabovac <ag@192.168.1.25>
2025-03-25 20:13:15 +0100

    Searchbar of header component can save the value of the query in state

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,13 @@
 "use client";
+
 import Image from "next/image";
 import Dropdown from "./drop-down-nav";
 import { useState } from 'react';
 
 export default function Header(){
     const [toggle, setToggle] = useState(0);
+    const [query, setQuery] = useState<any>(null);
+
     function toggleMenu(){
         if(toggle===0){
             setToggle(1)
@@ -12,7 +15,8 @@ export default function Header(){
             setToggle(0)
         }
     }
-    return <header className="text-black bg-amber-100 relative">
+
+    return <header className="text-black bg-amber-100 sticky top-0 z-10">
         <nav className="flex justify-between p-5 gap-5 items-center">
             <div className="flex gap-5 items-center">
                 <a href="/">
@@ -23,9 +27,9 @@ export default function Header(){
                     height={60}
                     />
                 </a>
-                <form className="flex">
-                    <input type="serarch" placeholder="Search here" className="w-[100%] border-1 border-neutral-500 mr-[.5px] rounded-l-full p-[.5rem] bg-orange-100"></input>
-                    <button className="w-[100%] flex-2 border-1 border-neutral-500 rounded-r-full p-[.5rem] bg-orange-300 text-orange-100">Search</button>
+                <form action={(data)=>{setQuery(data.get('query'))}} className="flex">
+                    <input name='query' type="serarch" placeholder="Search here" className="w-[100%] border-1 border-neutral-500 mr-[.5px] rounded-l-full p-[.5rem] bg-orange-100"></input>
+                    <button type="submit" className="w-[100%] flex-2 border-1 border-neutral-500 rounded-r-full p-[.5rem] bg-orange-300 text-orange-100">Search</button>
                 </form>
             </div>
             <div className="flex items-center gap-7">
