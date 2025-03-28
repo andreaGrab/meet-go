e26c722691a20bee2401a563b63bdfe4820dc46c
Andrea Grabovac <ag@192.168.1.45>
2025-03-28 14:36:26 +0100

    Added filter tags on demand logic.

--- src/components/events-filter-bar.tsx
+++ src/components/events-filter-bar.tsx
@@ -2,8 +2,14 @@
 import {useState} from "react";
 
 export default function EventsFilterBar(){
+    //styles
+    const filterTag = "border-1 border-rose-500 p-1 bg-rose-50 rounded-sm";
+
     const dateLocale = new Date();
-    const [date, setDate] = useState(dateLocale);
+    const [date, setDate]:any = useState(dateLocale);
+    const [category, setCategory]:any = useState('');
+    const [place, setPlace]:any = useState('');
+    const [isClear, setState]:any = useState(true);
 
     function formatDate(date:any){
         const dateLocaleSplited = date.toString().split(' ');
@@ -13,30 +19,70 @@ export default function EventsFilterBar(){
         const year = dateLocaleSplited[3];
         return `${year}-${month}-${day}`;
     }
-
-    function onSetDate(e:any){
-        setDate(new Date(e.target.value));
+ 
+    function onSetDate(dataObj:any){
+        setState(false);
+        const dataValue = dataObj.target.value;
+        setDate(new Date(dataValue));
+        console.log(isClear);
     }
+ 
+    function onSetCategory(dataObj:any){
+        const dataValue =  dataObj.target.value;
+        setCategory(dataValue);
+    }
+    function onSetPlace(dataObj:any){
+        const dataValue =  dataObj.target.value;
+        setPlace(dataValue);
+    }
+
+    console.log(isClear)
 
     return (
         <div className="flex flex-col">
             <div className="flex-1 flex justify-around">
-                <h1 className="text-2xl font-bold text-center md:mb-[5rem]">ALL EVENTS <span>filterd by: -sasasssas-</span></h1>
-                <a className="md:hidden" href="#"><img className="w-8" src="/reset-icon.svg" alt="reset icon" /></a>
+                <h1 className="text-2xl font-bold text-center md:mb-[5rem]">ALL EVENTS by:<br></br><span> <strong className={category!=''?filterTag:''}>{category}</strong> <strong className={place!=''?filterTag:''}>{place}</strong> <strong className={!isClear?filterTag:''}>{isClear?'':formatDate(date)}</strong></span></h1>
             </div>
             <div className="flex py-5 items-center justify-around gap-5 md:flex-1">
                 <img className="hidden md:block w-5" src="/filter.svg" alt="filter icon" />
-                <select className="border-1 border-black rounded-sm p-1" name="category">
-                    <option>Category</option>
-                </select>
-                <select className="border-1 border-black rounded-sm p-1" name="place">
-                    <option>Place</option>
-                </select>
-                <input className="border-1 border-black rounded-sm p-1" type="date" name="date" value={formatDate(date)} onChange={onSetDate}/>
-                <a className="hidden md:block" href="#"><img className="w-10" src="/reset-icon.svg" alt="reset icon" /></a>
-            </div>
-            <div className="text-right md:flex-1">
-
+                <form action={()=>{
+                    setCategory("");
+                    setPlace("");
+                    setDate(dateLocale);
+                    setState(true);
+                }} className="flex-1 flex gap-5 flex-col md:flex-row justify-between items-center">
+                    <select className="border-1 border-black md:rounded-sm p-3 text-center md:p-1" name="category" value={category} onChange={onSetCategory}>
+                        <option>Filter by category:</option>
+                        <option>Science</option>
+                        <option>Sport</option>
+                        <option>Music</option>
+                        <option>Gaming</option>
+                        <option>Comics</option>
+                        <option>Automotive/Motors</option>
+                        <option>Technology</option>
+                        <option>Cultural/Etnographic</option>
+                        <option>Arts and Crafts</option>
+                        <option>Entertainment</option>
+                        <option>Other</option>
+                    </select>
+                    <select className="border-1 border-black md:rounded-sm p-3 text-center" name="place" value={place} onChange={onSetPlace}>
+                        <option>Filter by place:</option>
+                        <option>Online</option>
+                        <option>Cina</option>
+                        <option>Japan</option>
+                        <option>Greece</option>
+                        <option>Turkey</option>
+                        <option>Italy</option>
+                        <option>Morocco</option>
+                        <option>Germany</option>
+                        <option>USA</option>
+                        <option>Canada</option>
+                        <option>...</option>
+                    </select>
+                    <input className="w-[100%] md:w-[initial] border-1 border-black md:rounded-sm p-3 text-center" type="date" name="date" value={formatDate(date)} onChange={onSetDate}/>
+                    <button type='submit' className="cursor-pointer"><img className="w-10" src="/reset-icon.svg" alt="reset icon" /></button>
+                </form>
+                
             </div>
         </div>
     )
